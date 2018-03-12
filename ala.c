435559da4de77b620a08e151eadf401f8d4f207a
Bartek Kirejczyk <bartek.kirej@gmail.com>
2018-03-12 17:53:39 +0100

    Add files via upload
    trzecie laborki z jipp. bawienie się stringami

---
+++ ala.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+	char ala[256] = "Ala ma kota";
+	
+	
+	//char zwierz[256];
+	//scanf("%s", &zwierz);	
+	//strcpy(ala + 7,zwierz);
+	//printf("%s", ala);
+	
+	
+	
+	//char *p = &ala;
+	//scanf("%s", (p + 7));
+	//printf("%s", ala);
+	
+	return 0;
+}
