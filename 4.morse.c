2bc3c8462e0c3df491ca3f09cf626cc6acc40a3b
Bartek Kirejczyk <bartek.kirej@gmail.com>
2018-03-31 00:00:17 +0200

    Add files via upload
    just the beginning. Now empty

---
+++ 4.morse.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(void)
+{
+	printf("Program kodujacy i dekodujacy alfabet Morse'a.\nWprowadz wiadomosc\n");
+	
+	char message[256];
+	
+	scanf("%s", message);
+	printf("%s", message);
+	
+	/*
+	miejsce na resztê programu
+	
+	*/
+	
+	return 0;
+}
