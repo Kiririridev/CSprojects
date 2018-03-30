0f235e7678bbf9d4b321664afeb4bc921f851a22
Bartek Kirejczyk <bartek.kirej@gmail.com>
2018-03-30 21:07:30 +0200

    Add files via upload
    final before refactorization

--- 3.szyfry.c
+++ 3.szyfry.c
@@ -25,7 +25,6 @@ void inputKey(int *pKey, int *pkeyLength)
 	
 	while(1)
 	{
-		//pogrzebany pies
 		
 		scanf("%d", &taker); 
 		
@@ -47,46 +46,166 @@ void inputKey(int *pKey, int *pkeyLength)
 	}
 }
 
-//main for coding input message
-int main()
+void codeMessageOld(char message[256], char *pCMes, int *pKey, int keyLength)
 {
-	char message[256];
-	char codedMessage[256];
-	int *pKey;
-	int keyLength = 0;
-	int *pkeyLength = &keyLength;
-	size_t size = 100;  
-	pKey = (int *)malloc(32);
-	char *pMes = &message[0]; 
 	int staticKey[keyLength];
 	int i = 0;
-
 	
-	inputMessage(pMes);
-	printf("test main: %s", message);
-	
-	inputKey(pKey, pkeyLength);
-	
-		
 	for(i = 0; i<keyLength; i++)
 	{
 		staticKey[i] = *(pKey + i);
 	}
 	
+	printf("key: ");
 	for(i = 0; i<keyLength; i++)
 	{
-		printf("key: %d", staticKey[i]);
-	}
+		printf(" %d ", staticKey[i]);
+	}printf("\n");
 	
-	
+
 	for(i = 0; i<(strlen(message)-1); i++)
+	{		
+		*(pCMes + i) = message[(i+staticKey[i%keyLength])%(strlen(message)-1)];
+		
+	}
+}
+
+void codeMessage(char message[256], char *pCMes, int *pKey, int keyLength)
+{
+	int i = 0;
+	
+	for(i = 0; i <strlen(message)-1; i++)
 	{
-		codedMessage[i] = message[(i+staticKey[i%keyLength])%(strlen(message)-1)];
+		*(pCMes + i) = message[i] + *(pKey+(i%keyLength));
+	}
+}
+
+
+
+//main for coding input message
+int mainCodeMessage()
+{
+	printf("Coding\n");
+	char message[256];
+	char codedMessage[256];
+	
+	int *pKey;
+	int keyLength = 0;
+	int *pKeyLength = &keyLength;
+	size_t size = 100;  
+	pKey = (int *)malloc(32);
+	
+	char *pMes = &message[0];
+	char *pCMes = &codedMessage[0]; 
+	int staticKey[keyLength];
+	int i = 0;
+
+	
+	
+	
+	inputMessage(pMes);
+	inputKey(pKey, pKeyLength);
+	codeMessage(message, pCMes, pKey, keyLength);
+
+	
+	printf("Coded message in main: %s\n", codedMessage);
+	
+	
+}
+
+void inputCodedMessage(char *pCMes)
+{
+	fflush(stdin);
+	printf("Enter your Message:");
+	fgets(pCMes, 256, stdin);
+	printf("InputCodedMessage: %s\n", pCMes);
+	
+}
+
+void uncodeMessageOld(char codedMessage[256], char *pMes, int *pKey, int keyLength)
+{
+	
+	char message[256];
+	int i = 0;
+	int staticKey[10];
+	
+	
+	for(i = 0; i<keyLength; i++)
+	{
+		staticKey[i] = *(pKey + i);
 	}
 	
-	printf("coded message: %s", codedMessage);
+
 	
+	printf("key: ");
+	for(i = 0; i<keyLength; i++)
+	{
+		printf(" %d ", staticKey[i]);
+	}printf("\n");
 	
+
+	int celM;
+	
+	for(i = 0; i<strlen(codedMessage)-1; i++)
+	{
+
+		celM = (i+staticKey[i%3])%6;
+		message[celM] = codedMessage[i];
+			
+	}
+		
+	printf("in uncode: %s\n", codedMessage);
+	printf("in uncode: %s\n", message);
+}
+
+void uncodeMessage(char codedMessage[256], char *pMes, int *pKey, int keyLength)
+{
+	int i = 0;
+	
+	for(i = 0; i <strlen(codedMessage)-1; i++)
+	{
+		*(pMes + i) = codedMessage[i] - *(pKey+(i%keyLength));
+	}
+}
+
+int mainUncodeMessage()
+{
+	printf("Uncoding\n");
+	char codedMessage[256];
+	char message[256];
+	
+	char *pCMes = &codedMessage[0];
+	char *pMes = &message[0];
+	
+	int key[10];
+	int *pKey = &key[0];
+	int keyLength;
+	int *pKeyLength = &keyLength;
+	
+	inputCodedMessage(pCMes);
+	inputKey(pKey, pKeyLength);
+	
+	uncodeMessage(codedMessage, pMes, pKey, keyLength);
+	
+	printf("\n%s", pMes);
+}
+
+int main()
+{	
+
+	char czarik = getch();
+	
+	loopLabel:
+	
+	printf("coding(c)/uncoding(u)?\n");
+	czarik = getch();
+	
+	if(czarik=='c')mainCodeMessage();
+	if(czarik=='u')mainUncodeMessage();
+	czarik = ' ';
+	sleep(1);
+	goto loopLabel;
+
 }
 
 
