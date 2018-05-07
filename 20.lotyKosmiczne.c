11171e142c96c13c77239d730ead5f4f09fe11e7
Bartek Kirejczyk <bartek.kirej@gmail.com>
2018-05-07 19:27:02 +0200

    Add files via upload
    createGalaxy works, but give some wrong names to planet. Look at pointers to pPlanetList and it's mallock

--- 20.lotyKosmiczne.c
+++ 20.lotyKosmiczne.c
@@ -5,17 +5,29 @@
 #include<time.h>
 #include"TestLotyKosmiczne.h"
 
-//extern void testNameTableInitializator(char*);
+extern void testNameTableInitializator(char*);
 extern void testNameTableInitializator2(void (*funcNameTableInitializator)(char*));
 extern void testRandomInteger(int (*funcRandInt)(int, int));
 extern void testNameGenerator(void (*funcNameGenerator)(char* pName, char* pNameTable), void (*funcNameTableInitializator)(char*));
+extern void testCreatePlanet(void (*funcCreatePlanet)(struct Planet*, char* pNameTable), char* pNameTable);
+extern void testCreateGalaxy(void funcCreateGalaxy(struct Planet*, char*), char* pNameTable, struct Planet* pPlanetList);
 
-
+/*
+*Generator liczb losowych w zakresie [from, to)
+*
+*/
 int randomInteger(int from, int to)
 {
 	return  from + (rand()%(to - from));
 }
 
+
+/*
+*Funkcja wype³nia tablice sylab, która póŸniej wykorzystywana jest przez generator nazw planet.
+*Wszyskitesylaby s¹ dwuliterowe, sk³adaj¹ siê z samogoski(lub a) oraz litery a. 
+*@param pNameTable - wskaŸnik do tablicy sylab. W tablicy powinniœmy mieæ zamallocowane 
+*przynajniej (10 * 2 * sizeof(char))
+*/
 void nameTableInitializator(char* pNameTable)
 {	
 	
@@ -36,11 +48,19 @@ void nameTableInitializator(char* pNameTable)
 	{
 		*(pNameTable + (2 * i) + 1) = 'a';
 	}
-	
-
 }
 
 
+
+/*
+*Generator imion wykorzystuj¹cy stworzon¹ wczeœniej tablicê z sylabami.
+*Tablica potrzebuje wskaŸnika do pierwszego elementu ³añcucha znaków oraz wskaŸnik do wczesniej
+*wype³nionej tablicy sylab(s³ownika).
+*
+*@param pName - wskaŸnik do ³añcucha znaków z imieniem. Potrzebuje zamallocowane przynajmniej (6 * sizeof(char))
+*@param pNameTable - wskaŸnik do tablicy sylab wype³nionej przez metodê void .nameTableInitializator(char* pNameTable)
+*
+*/
 void nameGenerator(char* pName, char* pNameTable)
 {
 	int i = 0, randomNumber;
@@ -53,6 +73,15 @@ void nameGenerator(char* pName, char* pNameTable)
 	}
 }
 
+
+/*
+* "Konstruktor" dla struct Planet. Tworzy planetê i nadaje jej losowe wartoœci koordynatów X,Y,Z oraz to czy jest gazowa.
+* Wywoluje metodê nameGenerator(char* pName, char* pNameTable), by stworzyæ losowe imie dla planety.
+*Metode potrzebuje wskaxnika do wczesniej stworzonej planet oraz wskaŸnika do tablicy sylab(s³ownika)
+*
+*@param pPlanet - wskaŸnik do miejsca w pamiêci, gdzie stworzymy planetê.
+*@param pNameTable - wskaŸnik do wype³nionej wczesniej przez funkcje .nameTableInitializator(char* pNameTable) tablicê sylab
+*/
 void createPlanet(struct Planet* pPlanet, char* pNameTable)
 {
 	char* pPlanetName = ((*(pPlanet)).name);
@@ -64,6 +93,18 @@ void createPlanet(struct Planet* pPlanet, char* pNameTable)
 	
 	(*pPlanet).isGas = randomInteger(0, 2);
 	
+}
+
+
+void createGalaxy(struct Planet* pPlanetTable, char* pNameTable)
+{
+	int i = 0;
+	for(i=0;i<10;i++)
+	{
+		createPlanet((pPlanetTable + (i * sizeof(struct Planet))), pNameTable);
+	}
+	
+
 }
 
 
@@ -83,19 +124,25 @@ void testy()
 	void (*funcNameTableInitializator)(char*) = &nameTableInitializator;
 	void (*funcNameGenerator)(char*, char*) = &nameGenerator;
 	void (*funcCreatePlanet)(struct Planet*, char* pNameTable) = &createPlanet;
+	void (*funcCreateGalaxy)(struct Planet*, char* pNameTable) = &createGalaxy;
 	
 	
 	char *pNameTable = malloc (10 * 2 * sizeof(char));
 	nameTableInitializator(pNameTable);
+	//struct Planet planetList[10];
+	struct Planet* pPlanetList = malloc(10* sizeof(struct Planet));
+
 	
 	
 	testRandomInteger(funcRandInt);
 	testNameTableInitializator2(funcNameTableInitializator);
 	testNameGenerator(funcNameGenerator, funcNameTableInitializator);
 	testCreatePlanet(funcCreatePlanet, pNameTable);
+	testCreateGalaxy(funcCreateGalaxy, pNameTable, pPlanetList);
 
 	
 	free(pNameTable);
+	free(pPlanetList);
 }
 
 //=========================MAIN================================
@@ -105,5 +152,5 @@ int main()
 	
 	srand(time(NULL));
 	testy();
-
+	
 }
--- TestLotyKosmiczne.h
+++ TestLotyKosmiczne.h
@@ -5,11 +5,9 @@
 #include<time.h>
 
 
-
-
 struct Planet
 {
-	char name[30]; 
+	char name[6]; 
 	
 	//Coordinates
 	int cordX;
@@ -54,8 +52,6 @@ void testRandomInteger(int (*funcRandInt)(int, int))
 }
 
 
-
-
 //PASSED
 void testNameGenerator(void (*funcNameGenerator)(char* pName, char* pNameTable), void (*funcNameTableInitializator)(char*))
 {
@@ -83,8 +79,7 @@ void testNameGenerator(void (*funcNameGenerator)(char* pName, char* pNameTable),
 
 //PASSED
 void testCreatePlanet(void funcCreatePlanet(struct Planet*, char* pNameTable), char* pNameTable)
-{
-	
+{	
 	int i = 0;
 	printf("\ntestCreatePlanet: \n");
 	struct Planet planet;
@@ -95,5 +90,23 @@ void testCreatePlanet(void funcCreatePlanet(struct Planet*, char* pNameTable), c
 		funcCreatePlanet(&planet, pNameTable);
 		printf("Planet name: %s, cordX: %d, cordY: %d, cordZ: %d, isGas: %d\n", planet.name, planet.cordX, planet.cordY, planet.cordZ, planet.isGas);
 	}
-
 }
+
+void testCreateGalaxy(void funcCreateGalaxy(struct Planet*, char*), char* pNameTable, struct Planet* pPlanetList)
+{
+	int i = 0;
+	printf("\ntestCreateGalaxy:\n");
+	funcCreateGalaxy(pPlanetList, pNameTable);
+	struct Planet planet; 
+	
+	for(i = 0; i<10; i++)
+	{
+		planet = (*(pPlanetList + (i * sizeof(struct Planet))));
+		printf("PlanetName: %s, cordX: %d, cordY: %d, cordZ: %d, isGas: %u\n", planet.name, planet.cordX, planet.cordY, planet.cordZ, planet.isGas);
+	}
+}
+	
+
+
+
+
